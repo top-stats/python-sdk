420674a8606b843995d41d4829bd6400cf67b08e
Arthurdw <arthur.dewitte@gmail.com>
2020-11-20 22:57:36 +0100

    Revert "Docs retry"
    This reverts commit c83323547ea527cbb5be1dcaa9aaf15625111774.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 import re
+import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath('.'))
 
@@ -36,7 +37,8 @@ release = version
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc'
+    'sphinx.ext.autodoc',
+    "sphinx_rtd_theme"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -53,7 +55,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'bizstyle'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
