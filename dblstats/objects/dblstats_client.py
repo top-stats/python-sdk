85ede4a5ea56027ce3392b1a3c433819eb42344b
Arthurdw <arthur.dewitte@gmail.com>
2020-11-20 14:26:33 +0100

    Added client.get_bot

--- dblstats/objects/dblstats_client.py
+++ dblstats/objects/dblstats_client.py
@@ -19,8 +19,12 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+from datetime import datetime
+from typing import Union
+
+from .bot import Bot
 from .dblstats_auctions import Auctions
-from ..utils import represents, AsyncFetcher
+from ..utils import represents, AsyncFetcher, endpoints
 
 
 class Client:
@@ -36,3 +40,18 @@ class Client:
 
     def __repr__(self):
         return represents(self)
+
+    async def get_bot(self, id: Union[int, str]) -> Bot:
+        """
+        Fetch a bot from the dblstatistics website.
+
+        :param id: The id of the bot that must be fetched.
+        """
+        bot = await self.__fetcher.get(endpoints.GET_BOT_URL.format(id=id))
+        return Bot(bot["certified"], list(map(lambda i: int(i), bot.get("owners"))), bot.get("deleted"), bot.get("id"),
+                   bot.get("name"), bot.get("def_avatar"), bot.get("avatar"), bot.get("short_desc"), bot.get("lib"),
+                   bot.get("prefix"), bot.get("website"),
+                   datetime.strptime(bot.get("approved_at"), "%Y-%m-%dT%H:%M:%S.%fZ"), bot.get("monthly_votes"),
+                   bot.get("server_count"), bot.get("total_votes"), bot.get("shard_count"),
+                   bot.get("monthly_votes_rank"), bot.get("server_count_rank"), bot.get("total_votes_rank"),
+                   bot.get("shard_count_rank"), datetime.strptime(bot.get("timestamp"), "%Y-%m-%dT%H:%M:%S.%fZ"))
