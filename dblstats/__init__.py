9d26c545f69a1f739f41b8b97c7a9bd4b540eff5
null8626 <vierofernando9@gmail.com>
2024-09-28 19:39:18 +0700

    meta: update project metadata

--- dblstats/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .client import Client
-
-__title__ = 'dblstats'
-__author__ = 'null8626'
-__license__ = 'MIT'
-__version__ = '1.0.0'
-__all__ = ('Client',)
--- topstats/__init__.py
+++ topstats/__init__.py
@@ -1,3 +1,4 @@
+"""
 The MIT License (MIT)
 
 Copyright (c) 2020 Arthurdw
@@ -20,3 +21,13 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+"""
+
+from .client import Client
+
+__title__ = 'topstats'
+__author__ = 'null8626'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2020 Arthurdw; Copyright (c) 2024 null8626'
+__version__ = '1.0.0'
+__all__ = ('Client',)
