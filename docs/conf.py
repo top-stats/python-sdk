6a709a87bacff5690a27801a283ff7abb6e707c2
null8626 <vierofernando9@gmail.com>
2025-02-15 15:17:49 +0700

    doc: update docs.topstats.gg links

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ intersphinx_mapping = {
 redirects = {
   'support-server': 'https://discord.com/invite/jjEauJXuZc',
   'repository': 'https://github.com/top-stats/dblstats.py',
-  'raw-api-reference': 'https://docs.topstats.gg/',
+  'raw-api-reference': 'https://docs.topstats.gg/introduction/introduction/',
 }
 
 html_css_files = [
