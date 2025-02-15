e848d314a06ec70f0503bec6fd35db9bf6bf7804
null8626 <vierofernando9@gmail.com>
2025-02-15 14:50:48 +0700

    meta: update copyright year

--- topstats/__init__.py
+++ topstats/__init__.py
@@ -2,7 +2,7 @@
 The MIT License (MIT)
 
 Copyright (c) 2020 Arthurdw
-Copyright (c) 2024 null8626
+Copyright (c) 2024-2025 null8626
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -37,7 +37,7 @@ from .errors import Error, RequestError, Ratelimited
 __title__ = 'topstats'
 __author__ = 'null8626'
 __license__ = 'MIT'
-__copyright__ = 'Copyright (c) 2020 Arthurdw; Copyright (c) 2024 null8626'
+__copyright__ = 'Copyright (c) 2020 Arthurdw; Copyright (c) 2024-2025 null8626'
 __version__ = '1.0.0'
 __all__ = (
   'Bot',
